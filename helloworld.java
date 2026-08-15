74fc851d2367768790978ec24168e3662d3d1e32
vishwa <jspvishwanath@gmail.com>
2026-08-15 11:47:51 +0530

    first commit

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {    public static void main(String[] args) {
+        System.out.println("Today is 15 August 2026.");
+        System.out.println("India celebrates its 79th Independence Day.");
+        System.out.println("India became independent on 15 August 1947.");
+        System.out.println("This day marks freedom from British rule.");
+        System.out.println("The first Prime Minister was Jawaharlal Nehru.");
+        System.out.println("Freedom fighters like Mahatma Gandhi, Bhagat Singh, Subhas Chandra Bose, and many others fought for independence.");
+        System.out.println("People celebrate with flags, speeches, parades, and patriotic songs.");
+    }
+}
