9e1f3ba2588f4309dffee7a0117b9312b7bd42e6
vishwadhotre09 <129178212+vishwadhotre09@users.noreply.github.com>
2026-08-15 14:11:58 +0530

    Update date in helloworld.java output message

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {    public static void main(String[] args) {
-        System.out.println("Today is 15 August 2026.");
+        System.out.println("Today is 15 August 2027.");
         System.out.println("India celebrates its 79th Independence Day.");
         System.out.println("India became independent on 15 August 1947.");
         System.out.println("This day marks freedom from British rule.");
